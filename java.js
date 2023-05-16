dc609b2615c2f7986d13c5fe944aee47d29d522a
teteulf <theolefevre791@gmail.com>
2023-05-15 22:27:30 -0300

    corrigido

--- java.js
+++ java.js
@@ -1,7 +1,16 @@
 var inp = document.querySelector("input");
+var p = document.querySelector("p");
+var select = document.querySelector("#select");
 
 inp.addEventListener("input", () => {
-  var p = document.querySelector("p");
-  let inputi = document.querySelector("input").value;
+  let inputi = inp.value;
   p.innerHTML = `Tabuada do ${inputi}`;
+  select.innerHTML = "";
+
+  for (let c = 1; c <= 10; c++) {
+    var num = Number(inp.value);
+    var opt = document.createElement("option");
+    opt.text = `${num} x ${c} = ${num * c}`;
+    select.appendChild(opt);
+  }
 });
